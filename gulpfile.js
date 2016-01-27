2120111c9e7be862426d3e9c27dc5e734077027b
smaximov <s.b.maximov@gmail.com>
2016-01-28 03:06:09 +1000

    Add Mocha & Chai for testing

--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,46 @@ var babel = require('gulp-babel');
 var webpack = require('webpack-stream');
 var concat = require('gulp-concat');
 var template = require('gulp-template');
+var mocha = require('gulp-mocha');
 
 var pkg = require('./package.json');
 
 gulp.task('build', function () {
-    return gulp.src('src/**/*.js')
-	.pipe(babel())
-	.pipe(gulp.dest('build'));
+  return gulp.src('src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('pack', ['build'], function () {
-    var webpackConfig = {
-	output: {
-	    filename: 'localtime.js'
-	}
-    };
-    return gulp.src('build/index.js')
-	.pipe(webpack(webpackConfig))
-	.pipe(gulp.dest('build'));
+  var webpackConfig = {
+    output: {
+      filename: 'localtime.js'
+    }
+  };
+  return gulp.src('build/index.js')
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('manifest', function () {
-    var bindings = {
-	name: pkg.description,
-	version: pkg.version
-    };
-    return gulp.src('src/manifest')
-	.pipe(template(bindings))
-	.pipe(gulp.dest('build'));
+  var bindings = {
+    name: pkg.description,
+    version: pkg.version
+  };
+  return gulp.src('src/manifest')
+    .pipe(template(bindings))
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('concat', ['manifest', 'pack'], function () {
-    return gulp.src(['build/manifest', 'build/localtime.js'])
-	.pipe(concat('localtime.js'))
-	.pipe(gulp.dest('.'));
+  return gulp.src(['build/manifest', 'build/localtime.js'])
+    .pipe(concat('localtime.js'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('test', ['build'], function () {
+  return gulp.src('test/**/test_*.js')
+    .pipe(mocha())
 });
 
 gulp.task('default', ['concat']);
