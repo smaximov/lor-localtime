238ba9d6cf2e6d755ccb50de710446344186abbe
smaximov <s.b.maximov@gmail.com>
2016-01-27 20:51:22 +1000

    Use webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var webpack = require('gulp-webpack');
 
 gulp.task('build', function () {
     return gulp.src('src/**/*.js')
@@ -8,4 +9,15 @@ gulp.task('build', function () {
 	.pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('pack', ['build'], function () {
+    var webpackConfig = {
+	output: {
+	    filename: 'localtime.js'
+	}
+    };
+    return gulp.src('build/index.js')
+	.pipe(webpack(webpackConfig))
+	.pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['pack']);
